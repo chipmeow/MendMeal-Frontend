2ca71a8e7222b8b2428648420438dee0246ce3a5
chipmeow <garimasingh102004@gmail.com>
2025-03-10 23:22:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
             // Here, you can display the recipes on the page
         })
         .catch(error => console.error("Error fetching recipes:", error));
+    if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js")
+            .then(reg => console.log("Service Worker Registered!", reg))
+            .catch(err => console.log("Service Worker Registration Failed!", err));
+    });
+}
+
 });
